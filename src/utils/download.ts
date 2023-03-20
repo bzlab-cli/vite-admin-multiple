2baff3ece5fa86c4c5310e0648fff70d202f9c1b
zhangbinjie <1435255141@qq.com>
2023-03-20 16:33:47 +0800

    update

---
+++ src/utils/download.ts
@@ -0,0 +1,43 @@
+import {
+  downloadAxiosBlobFile as downAxiosBlobFile,
+  downloadBlobFile as downBlobFile,
+  downloadLinkFile as downLinkFile,
+  isArray
+} from '@bzlab/bz-core'
+import { getToken } from '@/utils/auth'
+
+/**
+ * 通过访问Axios接口返回Blob下载文件，可指定文件名，多用于导出，下载模板
+ * downloadAxiosBlobFile({ url, name, data: {}, type: 'application/vnd.ms-excel' }) // 下载单个
+ * downloadAxiosBlobFile([{ url, name, data: {}, type: 'application/vnd.ms-excel' }]) // 下载多个
+ * @param options
+ */
+export function downloadAxiosBlobFile(options) {
+  const headers = { token: getToken() }
+  if (!isArray(options)) {
+    downAxiosBlobFile({ ...options, headers })
+  } else {
+    options.forEach(item => (item.headers = headers))
+    downAxiosBlobFile(options)
+  }
+}
+
+/**
+ * 将文件URL转成blob下载，可指定文件名，多用于浏览器无法触发自动下载的文件
+ * downloadBlobFile({ url, name }) // 下载单个
+ * downloadBlobFile([{ url, name }]) // 下载多个
+ * @param options
+ */
+export function downloadBlobFile(options) {
+  downBlobFile(options)
+}
+
+/**
+ * 文件直接下载，无需指定文件名，适用于直接打开地址触发下载文件
+ * downloadLinkFile(url) // 下载单个
+ * downloadLinkFile([url]) // 下载多个
+ * @param options
+ */
+export function downloadLinkFile(options) {
+  downLinkFile(options)
+}
