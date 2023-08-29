bb3857a949e42d6d7ad55b79164a2ec30cc1f7b4
zhangbinjie <1435255141@qq.com>
2023-08-29 10:50:58 +0800

    update

--- src/components/icon-selector/index.vue
+++ src/components/icon-selector/index.vue
@@ -10,14 +10,13 @@
       <template #reference>
         <el-input
           v-model="fontIconSearch"
-          :placeholder="fontIconPlaceholder"
+          placeholder="请选择图标"
           :clearable="clearable"
           :disabled="disabled"
           :size="size"
           style="width: 267px"
           ref="inputWidthRef"
           @clear="onClearFontIcon"
-          @focus="onIconFocus"
           @blur="onIconBlur"
         >
           <template #prepend>
@@ -71,42 +70,34 @@ export default {
   name: 'iconSelector',
   emits: ['update:modelValue', 'get', 'clear'],
   props: {
-    // 输入框前置内容
     prepend: {
       type: String,
       default: () => 'Menu'
     },
-    // 输入框占位文本
     placeholder: {
       type: String,
       default: () => '请输入内容搜索图标或者选择图标'
     },
-    // 输入框占位文本
     size: {
       type: String,
       default: () => 'default'
     },
-    // 弹窗标题
     title: {
       type: String,
       default: () => '请选择图标'
     },
-    // icon 图标类型
     type: {
       type: String,
       default: () => 'ele'
     },
-    // 禁用
     disabled: {
       type: Boolean,
       default: () => false
     },
-    // 是否可清空
     clearable: {
       type: Boolean,
       default: () => true
     },
-    // 自定义空状态描述文字
     emptyDescription: {
       type: String,
       default: () => '无相关图标'
@@ -121,49 +112,33 @@ export default {
       fontIconWidth: 0,
       fontIconSearch: '',
       fontIconTabsIndex: 0,
-      fontIconSheetsList: [],
-      fontIconPlaceholder: ''
+      fontIconSheetsList: []
     })
-    // 处理 input 获取焦点时，modelValue 有值时，改变 input 的 placeholder 值
-    const onIconFocus = () => {
-      if (!props.modelValue) return false
-      state.fontIconSearch = ''
-      state.fontIconPlaceholder = props.modelValue
-    }
-    // 处理 input 失去焦点时，为空将清空 input 值，为点击选中图标时，将取原先值
+
     const onIconBlur = () => {
-      setTimeout(() => {
-        const icon = state.fontIconSheetsList.filter((icon: string) => icon === state.fontIconSearch)
-        if (icon.length <= 0) state.fontIconSearch = ''
-      }, 300)
+      const icon = state.fontIconSheetsList.filter((icon: string) => icon === state.fontIconSearch)
+      if (!icon.length) onClearFontIcon()
     }
-    // 处理 icon 双向绑定数值回显
     const initModeValueEcho = () => {
-      if (props.modelValue === '') return false
-      state.fontIconPlaceholder = props.modelValue
+      state.fontIconSearch = props.modelValue
       state.fontIconPrefix = props.modelValue
     }
-    // 图标搜索及图标数据显示
     const fontIconSheetsFilterList = computed(() => {
-      if (!state.fontIconSearch) return state.fontIconSheetsList
-      let search = state.fontIconSearch.trim()
-      return state.fontIconSheetsList.filter((item: any) => {
-        if (item.indexOf(search) !== -1) return item
-      })
+      return state.fontIconSheetsList
+      // if (!state.fontIconSearch) return state.fontIconSheetsList
+      // let search = state.fontIconSearch.trim()
+      // return state.fontIconSheetsList.filter((item: any) => {
+      //   if (item.indexOf(search) !== -1) return item
+      // })
     })
-    // 获取 input 的宽度
     const getInputWidth = () => {
       nextTick(() => {
         state.fontIconWidth = inputWidthRef.value.$el.offsetWidth
       })
     }
-    // 监听页面宽度改变
     const initResize = () => {
-      window.addEventListener('resize', () => {
-        getInputWidth()
-      })
+      window.addEventListener('resize', () => getInputWidth())
     }
-    // 初始化数据
     const initFontIconData = async () => {
       if (props.type === 'ele') {
         await initIconfont.ele().then((res: any) => {
@@ -171,30 +146,26 @@ export default {
           state.fontIconSheetsList = res
         })
       }
-      state.fontIconPlaceholder = props.placeholder
+      state.fontIconSearch = props.placeholder
       initModeValueEcho()
     }
-    // 获取当前点击的 icon 图标
     const onColClick = (v: any) => {
-      state.fontIconPlaceholder = v
+      state.fontIconSearch = v
       state.fontIconVisible = false
       if (state.fontIconTabsIndex === 1) state.fontIconPrefix = `${v}`
       emit('get', state.fontIconPrefix)
       emit('update:modelValue', state.fontIconPrefix)
     }
-    // 清空当前点击的 icon 图标
     const onClearFontIcon = () => {
       state.fontIconPrefix = ''
       emit('clear', state.fontIconPrefix)
       emit('update:modelValue', state.fontIconPrefix)
     }
-    // 页面加载时
     onMounted(() => {
       initFontIconData()
       initResize()
       getInputWidth()
     })
-    // 监听双向绑定 modelValue 的变化
     watch(
       () => props.modelValue,
       () => {
@@ -206,7 +177,6 @@ export default {
       fontIconSheetsFilterList,
       onColClick,
       onClearFontIcon,
-      onIconFocus,
       onIconBlur,
       ...toRefs(state)
     }
@@ -217,6 +187,12 @@ export default {
 <style lang="scss" scoped>
 .icon-selector {
   width: 100%;
+  :deep(.el-input-group__prepend) {
+    cursor: pointer;
+  }
+  :deep(.el-input__inner::selection) {
+    background-color: transparent;
+  }
 }
 .icon-selector-wrap {
   .icon-selector-wrap-row {
