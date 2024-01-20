197858ceedd9af1612a03d2b6ab41865fe4e5806
JieRucker <1435255141@qq.com>
2024-01-20 14:31:16 +0800

    update

--- src/views/admin/router/constant-modules/dashboard.ts
+++ src/views/admin/router/constant-modules/dashboard.ts
@@ -3,7 +3,7 @@
  * @Description:
  * @Date: 2021/10/25 11:12:57
  * @LastEditors: jrucker
- * @LastEditTime: 2023/06/14 13:42:03
+ * @LastEditTime: 2024/01/20 14:30:17
  */
 
 import { RouteRecordRaw } from 'vue-router'
@@ -12,18 +12,22 @@ import Layout from '@/layout/admin/index.vue'
 const DashboardRouter: Array<RouteRecordRaw> = [
   {
     path: '/dashboard',
+    redirect: '/dashboard/index',
     component: Layout,
     meta: {
+      title: '首页',
+      icon: 'Menu',
       hidden: true
     },
     children: [
       {
-        path: 'dashboard',
+        path: '/dashboard/index',
         component: () => import('@/views/admin/dashboard/index.vue'),
-        name: 'dashboard',
+        name: 'dashboard-index',
         meta: {
           title: '首页',
-          icon: 'Menu'
+          icon: 'Menu',
+          hidden: true
         }
       }
     ]
